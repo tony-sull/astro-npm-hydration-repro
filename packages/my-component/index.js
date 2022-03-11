30b395659bf4e26b058ec92094f700e9fde6cd64
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-11 12:41:15 +0100

    adding a basic counter example

--- packages/my-component/index.js
+++ packages/my-component/index.js
@@ -1,2 +1,4 @@
 export { default as Button } from './Button.astro';
 export { default as Heading } from './Heading.astro';
+export { default as Counter } from './Counter.jsx';
+export { default as LazyCounter } from './LazyCounter.astro';
